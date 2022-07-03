123b33f2e7c54b6ce47c8c8c7a58fcafb8baac8c
Victor Abbah <93128571+devAbba@users.noreply.github.com>
2022-07-03 06:57:55 +0000

    attempted 2 0f 3

--- src/assignment.js
+++ src/assignment.js
@@ -14,7 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let n of arrayOfNumbers) {
+    sum += n;
+  }
+  return sum;
+    // return arrayOfNumbers.length;
 }
 // assignment.sumOfNumbers = sumOfNumbers;
 
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for (let n of arrayOfNumbers){
+    if (n % 2 === 0){
+      count += 1;
+    }
+  }
+  return count;
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +59,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  const newArr = arrayOfNumbers.map((num) => {
+    return Math.trunc(num * 9/5 + 32);
+  })
+  return newArr;
+    // return arrayOfNumbers;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
